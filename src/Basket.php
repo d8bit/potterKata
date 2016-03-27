a1536c7948fddad37019af61f10beb135af194f3
Deibit <sickboy140@hotmail.com>
2016-03-27 14:59:44 +0200

    first import

---
+++ src/Basket.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This file contains Basket class
+ */
+
+
+class Basket
+{
+    private $_books = [];
+    const PRICE = 8;
+
+    /**
+     * Adds a book
+     *
+     * @param string $title Title of the book
+     *
+     * @return void
+     */
+    public function addBook($title)
+    {
+        $inserted = false;
+        if ($this->_cartIsEmpty()) {
+            $this->_books[0][] = $title;
+        } else {
+            for ($i = 0; $i < count($this->_books); $i++) {
+                if (!in_array($title, $this->_books[$i])) {
+                    $this->_books[$i][] = $title;
+                    $inserted = true;
+                    break;
+                }
+            }
+            if (!$inserted) {
+                $this->_books[count($this->_books)][] = $title;
+            }
+        }
+
+    }
+
+    /**
+     * Checks if cart is empty
+     *
+     * @return boolean
+     */
+    private function _cartIsEmpty()
+    {
+        return (count($this->_books) == 0);
+    }
+
+    /**
+     * Returns books
+     *
+     * @return array Array of books
+     */
+    public function getBooks()
+    {
+        return $this->_books;
+    }
+
+    /**
+     * Return number of books
+     *
+     * @return int
+     */
+    public function getBooksNumber()
+    {
+        $num = 0;
+        foreach ($this->_books as $books_number) {
+            $num += count($books_number);
+        }
+        return $num;
+    }
+
+    /**
+     * Get total account
+     *
+     * @return int
+     */
+    public function getTotal()
+    {
+        $total = 0;
+        foreach ($this->_books as $group_books) {
+            $n_distinct_books = count($group_books);
+            $total_group = count($group_books) * self::PRICE;
+
+            switch($n_distinct_books) {
+            case 2:
+                $total_group = $total_group * 0.95;
+                break;
+            case 3:
+                $total_group = $total_group * 0.90;
+                break;
+            case 4:
+                $total_group = $total_group * 0.80;
+                break;
+            case 5:
+                $total_group = $total_group * 0.75;
+                break;
+            }
+            $total += $total_group;
+        }
+
+        return number_format($total, 2);
+    }
+
+}
---
+++ test/BasketTest.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once 'src/Basket.php';
+
+class BasketTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testGetBooksNumber()
+    {
+        $basket = new Basket();
+        $basket->addBook('book1');
+        $this->assertEquals(1, $basket->getBooksNumber());
+        $basket->addBook('book2');
+        $this->assertEquals(2, $basket->getBooksNumber());
+        $basket->addBook('book2');
+        $this->assertEquals(3, $basket->getBooksNumber());
+    }
+
+    public function testGetTotal()
+    {
+        $basket = new Basket();
+        $basket->addBook('book1');
+        $this->assertEquals(8, $basket->getTotal());
+        $basket->addBook('book2');
+        $expect = 2 * 8 * 0.95;
+        $this->assertEquals($expect, $basket->getTotal());
+        $basket->addBook('book3');
+        $expect = 3 * 8 * 0.90;
+        $this->assertEquals($expect, $basket->getTotal());
+        $basket->addBook('book4');
+        $expect = 4 * 8 * 0.80;
+        $this->assertEquals($expect, $basket->getTotal());
+        $basket->addBook('book5');
+        $expect = 5 * 8 * 0.75;
+        $this->assertEquals($expect, $basket->getTotal());
+        $basket->addBook('book5');
+        $expect = (5 * 8 * 0.75) + 8;
+        $this->assertEquals($expect, $basket->getTotal());
+        $basket->addBook('book5');
+        $expect = (5 * 8 * 0.75) + (2*8);
+        $this->assertEquals($expect, $basket->getTotal());
+    }
+
+    public function copiedTest()
+    {
+        $basket = new Basket();
+        $basket->addBook('book0');
+        $basket->addBook('book1');
+        $basket->addBook('book1');
+        $basket->addBook('book2');
+        $basket->addBook('book3');
+        $basket->addBook('book4');
+        $this->assertEquals(8 + (8 * 5 * 0.75), $basket->getTotal());
+    }
+
+    public function testKata()
+    {
+        // 2 copies of the first book
+        // 2 copies of the second book
+        // 2 copies of the third book
+        // 1 copy of the fourth book
+        // 1 copy of the fifth book
+        $basket = new Basket();
+        $basket->addBook('book1');
+        $basket->addBook('book1');
+        $basket->addBook('book2');
+        $basket->addBook('book2');
+        $basket->addBook('book3');
+        $basket->addBook('book3');
+        $basket->addBook('book4');
+        $basket->addBook('book5');
+        // $expect = 2 * (8 * 4 * 0.8);
+        // $this->assertEquals($basket->getTotal(), $expect);
+    }
+
+}
+
