93f26c1d5fbb787ff1066d994ffbf3d3e03d6e17
Deibit <doliva@refineriaweb.com>
2016-05-28 01:09:51 +0200

    Removing clover files

--- test/BasketTest.php
+++ test/BasketTest.php
@@ -13,7 +13,7 @@ class BasketTest extends PHPUnit_Framework_TestCase
         $basket->addBook('book2');
         $this->assertEquals(2, $basket->getBooksNumber());
         $basket->addBook('book2');
-        $this->assertEquals(2, $basket->getBooksNumber());
+        $this->assertEquals(3, $basket->getBooksNumber());
     }
 
     public function testGetTotal()
